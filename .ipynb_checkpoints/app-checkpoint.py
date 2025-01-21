26bda7429dfda8e14021be3186c439db11e74991
amb-fex <sdefex@amb.cat>
2025-01-21 03:05:57 +0100

    Actualización en app.py, requirements.txt y formulario.html

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,19 +1,10 @@
 import psycopg2
 from flask import Flask, request, jsonify, render_template
-import os
 
 app = Flask(__name__)
 
 # URL de conexión externo de Render
 DATABASE_URL = "postgresql://btu_bim_user:yOs3ITKc3EKTKbF3Yx0gaOypi5g5HvI4@dpg-cu72o123esus73ffd48g-a/btu_bim"
-
-
-# Conexión a la base de datos
-try:
-    conn = psycopg2.connect(DATABASE_URL, sslmode="require")
-    print("Conexión exitosa a la base de datos")
-except Exception as e:
-    print(f"Error al conectar a la base de datos: {e}")
 
 # Ruta principal para servir el formulario HTML
 @app.route("/", methods=["GET"])
@@ -23,37 +14,37 @@ def home():
 # Ruta para manejar el envío del formulario
 @app.route("/submit", methods=["POST"])
 def submit_data():
-    # Obtener datos enviados desde el formulario
     data = request.json
-    categoria = data.get("categoria")
-    elemento = data.get("elemento")
-    descripcion = data.get("descripcion")
-    tipo = data.get("tipo")
-    geometria = data.get("geometria")
-    terreno = data.get("terreno")  # Esto será una lista
-    captacion = data.get("captacion")
-    fotografias = data.get("fotografias")  # Esto será Base64
-    observaciones = data.get("observaciones")
+    try:
+        categoria = data.get("categoria")
+        elemento = data.get("elemento")
+        descripcion = data.get("descripcion")
+        tipo = data.get("tipo")
+        geometria = data.get("geometria")
+        terreno = ','.join(data.get("terreno", []))  # Convierte lista a cadena
+        captacion = data.get("captacion")
+        fotografias = data.get("fotografias")
+        observaciones = data.get("observaciones")
+    except KeyError as e:
+        return jsonify({"error": f"Faltan datos requeridos: {e}"}), 400
 
     try:
-        # Usar la conexión global para insertar datos
+        # Conexión a la base de datos
+        conn = psycopg2.connect(DATABASE_URL, sslmode="require")
         cur = conn.cursor()
-
-        # Insertar datos en la tabla
         cur.execute("""
             INSERT INTO datos_btu_bim 
             (categoria, elemento_objeto, descripcion, tipo, geometria, terreno, captacion, fotografias, observaciones)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
         """, (categoria, elemento, descripcion, tipo, geometria, terreno, captacion, fotografias, observaciones))
         conn.commit()
-
         cur.close()
+        conn.close()
         return jsonify({"message": "Datos guardados con éxito"}), 200
     except Exception as e:
-        print(f"Error al guardar los datos: {e}")
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": f"Error al guardar los datos: {e}"}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
-  
+
 
